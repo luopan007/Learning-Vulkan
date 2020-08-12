a13930ba25948fe2c812130c37b510034a51658b
luopan <709165253@qq.com>
2020-08-12 16:36:02 +0800

    Update VulkanInstance.cpp

--- Chapter 04/DebugVulkan/source/VulkanInstance.cpp
+++ Chapter 04/DebugVulkan/source/VulkanInstance.cpp
@@ -1,36 +1,13 @@
-/*
-* Learning Vulkan - ISBN: 9781786469809
-*
-* Author: Parminder Singh, parminder.vulkan@gmail.com
-* Linkedin: https://www.linkedin.com/in/parmindersingh18
-*
-* Permission is hereby granted, free of charge, to any person obtaining a
-* copy of this software and associated documentation files (the "Software"),
-* to deal in the Software without restriction, including without limitation
-* the rights to use, copy, modify, merge, publish, distribute, sublicense,
-* and/or sell copies of the Software, and to permit persons to whom the
-* Software is furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included
-* in all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-* DEALINGS IN THE SOFTWARE.
-*/
 
 #include "VulkanInstance.h"
 
 VkResult VulkanInstance::createInstance(std::vector<const char *>& layers, std::vector<const char *>& extensionNames, char const*const appName)
 {
+	// 设置实例相关的层和扩展信息
 	layerExtension.appRequestedExtensionNames	= extensionNames;
 	layerExtension.appRequestedLayerNames		= layers;
 	
-	// Define the Vulkan application structure 
+	// 定义Vulkan应用程序的结构体
 	VkApplicationInfo appInfo	= {};
 	appInfo.sType				= VK_STRUCTURE_TYPE_APPLICATION_INFO;
 	appInfo.pNext				= NULL;
@@ -38,22 +15,22 @@ VkResult VulkanInstance::createInstance(std::vector<const char *>& layers, std::
 	appInfo.applicationVersion	= 1;
 	appInfo.pEngineName			= appName;
 	appInfo.engineVersion		= 1;
-	// VK_API_VERSION is now deprecated, use VK_MAKE_VERSION instead.
-	appInfo.apiVersion			= VK_MAKE_VERSION(1, 0, 0);
+	appInfo.apiVersion			= VK_MAKE_VERSION(1, 2, 0);
 
-	// Define the Vulkan instance create info structure 
+	// 定义Vulkan实例创建的参数结构体
 	VkInstanceCreateInfo instInfo	= {};
 	instInfo.sType					= VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-	instInfo.pNext					= &layerExtension.dbgReportCreateInfo;
+	// 为了确保vkCreateInstance执行的时候就已经开启调试功能，所以讲调试报告的结构体传递给VkInstanceCreateInfo的pNext。
+	instInfo.pNext					= &layerExtension.dbgReportCreateInfo; 
 	instInfo.flags					= 0;
 	instInfo.pApplicationInfo		= &appInfo;
 
-	// Specify the list of layer name to be enabled.
-	instInfo.enabledLayerCount		= (uint32_t)layers.size();
+	// 设置需要启用的层名称的列表
+	instInfo.enabledLayerCount		= static_cast<uint32_t>(layers.size());
 	instInfo.ppEnabledLayerNames	= layers.size() ? layers.data() : NULL;
 
-	// Specify the list of extensions to be used in the application.
-	instInfo.enabledExtensionCount	= (uint32_t)extensionNames.size();
+	// 设置需要启用的扩展名称列表
+	instInfo.enabledExtensionCount	= static_cast<uint32_t>(extensionNames.size());
 	instInfo.ppEnabledExtensionNames = extensionNames.size() ? extensionNames.data() : NULL;
 
 	VkResult result = vkCreateInstance(&instInfo, NULL, &instance);
