5d880556d9fcc045195ab26e0f5aac527eaebd8a
luopan <709165253@qq.com>
2020-08-12 16:34:36 +0800

    Update VulkanDevice.h

--- Chapter 04/DebugVulkan/include/VulkanDevice.h
+++ Chapter 04/DebugVulkan/include/VulkanDevice.h
@@ -1,27 +1,3 @@
-/*
-* Learning Vulkan - ISBN: 9781786469809
-*
-* Author: Parminder Singh, parminder.vulkan@gmail.com
-* Linkedin: https://www.linkedin.com/in/parmindersingh18
-*
-* Permission is hereby granted, free of charge, to any person obtaining a
-* copy of this software and associated documentation files (the "Software"),
-* to deal in the Software without restriction, including without limitation
-* the rights to use, copy, modify, merge, publish, distribute, sublicense,
-* and/or sell copies of the Software, and to permit persons to whom the
-* Software is furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included
-* in all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-* DEALINGS IN THE SOFTWARE.
-*/
 
 #pragma once
 
@@ -43,29 +19,27 @@ public:
 	~VulkanDevice();
 
 public:
-    VkDevice							device;		// Logical device
-	VkPhysicalDevice*					gpu;		// Physical device
-	VkPhysicalDeviceProperties			gpuProps;	// Physical device attributes
+    VkDevice							device;		// 逻辑设备
+	VkPhysicalDevice*					gpu;		// 物理设备
+	VkPhysicalDeviceProperties			gpuProps;	// 物理设备属性
     VkPhysicalDeviceMemoryProperties	memoryProperties;
 
 public:
-	// Queue
-	VkQueue									queue;							// Vulkan Queues object
-	std::vector<VkQueueFamilyProperties>	queueFamilyProps;				// Store all queue families exposed by the physical device. attributes
-	uint32_t								graphicsQueueIndex;				// Stores graphics queue index
-	uint32_t								graphicsQueueWithPresentIndex;  // Number of queue family exposed by device
-	uint32_t								queueFamilyCount;				// Device specificc layer and extensions
+	// 队列相关属性
+	VkQueue									queue;							// Vulkan队列属性
+	std::vector<VkQueueFamilyProperties>	queueFamilyProps;				// 保存物理设备暴露的所有队列族
+	uint32_t								graphicsQueueIndex;				// 保存图形队列的索引号
+	uint32_t								graphicsQueueWithPresentIndex;  // 
+	uint32_t								queueFamilyCount;				// 设备中队列的数量
 
-	// Layer and extensions
+	// 设备相关的扩展
 	VulkanLayerAndExtension		layerExtension;
 
 public:
 	VkResult createDevice(std::vector<const char *>& layers, std::vector<const char *>& extensions);
 	void destroyDevice();
 
-	// Get the avaialbe queues exposed by the physical devices
 	void getPhysicalDeviceQueuesAndProperties();
 
-	// Query physical device to retrive queue properties
 	uint32_t getGraphicsQueueHandle();
 };
