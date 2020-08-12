a16a5de6476fbba3c0484ee74ecb9406a22003bc
luopan <709165253@qq.com>
2020-08-12 15:15:24 +0800

    Update main.cpp

--- Chapter 03/HandShake/source/main.cpp
+++ Chapter 03/HandShake/source/main.cpp
@@ -1,28 +1,3 @@
-/*
-* Learning Vulkan - ISBN: 9781786469809
-*
-* Author: Parminder Singh, parminder.vulkan@gmail.com
-* Linkedin: https://www.linkedin.com/in/parmindersingh18
-*
-* Permission is hereby granted, free of charge, to any person obtaining a
-* copy of this software and associated documentation files (the "Software"),
-* to deal in the Software without restriction, including without limitation
-* the rights to use, copy, modify, merge, publish, distribute, sublicense,
-* and/or sell copies of the Software, and to permit persons to whom the
-* Software is furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included
-* in all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-* DEALINGS IN THE SOFTWARE.
-*/
-
 #include "Headers.h"
 #include "VulkanApplication.h"
 
@@ -41,6 +16,7 @@ std::vector<const char *> deviceExtensionNames = {
 
 int main(int argc, char **argv)
 {
+	// 创建单例对象，调用构造函数
 	VulkanApplication* appObj = VulkanApplication::GetInstance();
 	appObj->initialize();
 	appObj->prepare();
